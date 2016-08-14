034c7c654aad8d8e48a1c9cf9021276d52c7b8b7
zotakk4o <zotak3105@gmail.com>
2016-08-14 09:59:50 +0300

    Details For Every Post Added

---
+++ GameBG/details.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'header.php';
+include_once 'connection.php';
+session_start();
+?>
+<?php
+$id = $_GET['id'];
+$statement = $connection->query('SELECT * FROM posts WHERE id = '.$id.'');
+$row = $statement->fetch_assoc();
+?>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Content</th>
+        <th>Date</th>
+        <td>Tag</td>
+        <td>Author ID</td>
+        <th>Action</th>
+    </tr>
+        <tr>
+            <td><?=$row['id']?></td>
+            <td><?=htmlspecialchars($row['title'])?></td>
+            <td><?=$row['content']?></td>
+            <td><?=htmlspecialchars($row['date'])?></td>
+            <td><?=htmlspecialchars($row['tag'])?></td>
+            <td><?=$row['author_id']?></td>
+            <td><a href="blog.php">[Back to Blog]</a></td>
+           <?php if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+            if($row['author_id'] == $_SESSION['user_id'] || $_SESSION['username'] == "admin") {?>
+            <td><a href="#">[Edit]</a>
+                <a href="#">[Delete]</a>
+            </td>
+            <?php }}?>
+        </tr>
+</table>
+<?php include_once 'footer.php' ?>
--- GameBG/pc-gaming.php
+++ GameBG/pc-gaming.php
@@ -17,23 +17,36 @@ function cutLongText($text,  $maxSize=200,  $htmlEscape = true)
     return $text . $append;
 }
 ?>
-<?php foreach($statement as $post) :?>
+<table>
     <tr>
-        <td><?=$post['id']?></td>
-        <td><?=htmlspecialchars($post['title'])?></td>
-        <td><?=cutLongText($post['content'])?></td>
-        <td><?=htmlspecialchars($post['date'])?></td>
-        <td><?=htmlspecialchars($post['tag'])?></td>
-        <td><?=$post['author_id']?></td>
-        <?php
-        if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-            if($post['author_id'] == $_SESSION['user_id'] || $_SESSION['username'] == "admin") {?>
-                <td><a href="#">[Edit]</a>
-                    <a href="#">[Delete]</a>
-                </td>
-            <?php }}?>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Content</th>
+        <th>Date</th>
+        <td>Tag</td>
+        <td>Author ID</td>
+        <th>Action</th>
     </tr>
-<?php endforeach ?>
+    <?php foreach($statement as $post) :?>
+        <tr>
+            <td><?=$post['id']?></td>
+            <td><?=htmlspecialchars($post['title'])?></td>
+            <td><?=cutLongText($post['content'])?></td>
+            <td><?=htmlspecialchars($post['date'])?></td>
+            <td><?=htmlspecialchars($post['tag'])?></td>
+            <td><?=$post['author_id']?></td>
+            <td><a href="details.php?id=<?=$post['id']?>" name="details">[Details]</a></td>
+            <?php
+            if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+                if($post['author_id'] == $_SESSION['user_id'] || $_SESSION['username'] == "admin") {?>
+                    <td><a href="#">[Edit]</a>
+                        <a href="#">[Delete]</a>
+                    </td>
+                <?php }
+            }?>
+        </tr>
+    <?php endforeach ?>
+</table>
 <?php
 include_once 'footer.php';
 ?>
