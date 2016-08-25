ae58b22bd2d2bd1c755004104acc8b914bfac7ba
Petar Staynov <petar.staynov@gmail.com>
2016-08-25 19:08:08 +0300

    Footer changes
    Made footer smaller and stick to bottom

--- GameBG/footer.php
+++ GameBG/footer.php
@@ -1,28 +1,4 @@
 <footer>
-	<div class="footer-1">
-		<div class="footer-col">
-			<div>
-				<img src="images/footer-about.png">
-				<h1>За нас</h1>
-				<p>Ние от <span>GameBG</span> искаме да свържем хората които играят за да се забавляват заедно</p>
-			</div>
-		</div>
-		<div class="footer-col">
-			<div>
-				<img src="images/footer-message.png">
-				<h1>Нужда от помощ?</h1>
-				<p>support@gamebg.com</p>
-			</div>
-		</div>
-		<div class="footer-col">
-			<div>
-				<img src="images/footer-phone.png">
-				<h1>Обадете ни се</h1>
-				<p>+359 123 456 789</p>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
 	<div class="footer-2">
 		<div class="footer-2-container">
 			<div>
@@ -36,7 +12,6 @@
 			</div>
 		</div>
 	</div>
-	<div class="clearfix"></div>
 </footer>
 </body>
 </html>
