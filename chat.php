1cf2d6e9b05d7e5274bf4d50788ec38999d42338
hristo2612 <hristoapps@gmail.com>
2016-08-27 08:48:53 -0700

    Almost Final version of chat
    fixed fadein/fadeout, scroll glitchy effects when submitting a chat message and closing the chat. changed style of the chat .

---
+++ chat.php
@@ -0,0 +1,14 @@
+
+<div id="ChatContainer">
+    <div id="chatLogs" align="center" class="ScrollStyle">
+        <img id="loadingChatImage" src="loading.gif">
+    </div>
+    <div id="typeArea">
+        <form  id="messageForm" name="msgForm" style="text-align: center;">
+            <textarea id="msg"></textarea>
+            <br>
+            <a id="sendMsgButton" href="#">Send Message</a>
+            <br>
+        </form>
+    </div>
+</div>
---
+++ logs.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gamebg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT username, msg FROM logs";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $counter = 0;
+    while($row = $result->fetch_assoc()) {
+        if ($counter % 2 == 0){
+            echo "<span style='color:#c4f7ff;'>" . "* " . $row["username"]. ": " . $row["msg"] . "</span>" . "<br>";
+        }else {
+            echo "<span style='color:white;'>" . "* " . $row["username"] . ": " . $row["msg"] . "</span>" . "<br>";
+        }
+        $counter ++;
+    }
+}
