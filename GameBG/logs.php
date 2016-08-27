47116302edc28dc475e526a0158407415cf86e86
zotakk4o <zotak3105@gmail.com>
2016-08-28 00:43:19 +0300

    Guest User Header Bug Fixed

--- GameBG/logs.php
+++ GameBG/logs.php
@@ -16,13 +16,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    $counter = 0;
     while($row = $result->fetch_assoc()) {
-        if ($counter % 2 == 0){
-            echo "<span style='color:#c4f7ff;'>" . "* " . $row["username"]. ": " . $row["msg"] . "</span>" . "<br>";
-        }else {
-            echo "<span style='color:white;'>" . "* " . $row["username"] . ": " . $row["msg"] . "</span>" . "<br>";
-        }
-        $counter ++;
+        echo "* " . $row["username"] . ": " . $row["msg"] . "<br>";
     }
 }
