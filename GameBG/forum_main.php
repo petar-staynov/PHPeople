1cf493de2a9b344e76ebe5949c264c042064f4ee
Petar Staynov <petar.staynov@gmail.com>
2016-08-30 14:03:02 +0300

    Forum - categories on forum home

--- GameBG/forum_main.php
+++ GameBG/forum_main.php
@@ -1,18 +1,47 @@
 <?php
 $title = "Forum";
-include 'forum_connect.php';
 include 'forum-header.php';
 ?>
 
 <?php
-echo '<tr>';
-echo '<td class="leftpart">';
-echo '<h3><a href="category.php?id=">Category name</a></h3> Category description goes here';
-echo '</td>';
-echo '<td class="rightpart">';
-echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
-echo '</td>';
-echo '</tr>';
+require 'forum_connect.php';
+$sql = "SELECT cat_id, cat_name, cat_description FROM forum_categories";
+
+//FIX THIS
+$result = mysqli_query($con, $sql);
+
+if(!$result)
+{
+    echo 'The categories could not be displayed, please try again later.';
+}
+else
+{
+    if(mysqli_num_rows($result) == 0)
+    {
+        echo 'No categories defined yet.';
+    }
+    else
+    {
+        //prepare the table
+        echo '<table border="1">
+              <tr>
+                <th>Category</th>
+                <th>Last topic</th>
+              </tr>';
+
+        while($row = mysqli_fetch_assoc($result))
+        {
+            echo '<tr>';
+            echo '<td class="leftpart">';
+            echo '<h3><a href="category.php?id">' . $row['cat_name'] . '</a></h3>' . $row['cat_description'];
+            echo '</td>';
+            echo '<td class="rightpart">';
+            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
+            echo '</td>';
+            echo '</tr>';
+        }
+    }
+}
 ?>
 
 <?php
