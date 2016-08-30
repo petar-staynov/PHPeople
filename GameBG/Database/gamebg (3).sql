cf7d460a0d38e9f871c771a74c65e77b45803690
TheAngelM97 <miladinov_1997@abv.bg>
2016-08-30 19:53:12 +0300

    Database

---
+++ GameBG/Database/gamebg (3).sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 
+-- Версия на сървъра: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gamebg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `game_title` varchar(250) NOT NULL,
+  `game_desc` text NOT NULL,
+  `game_image` varchar(250) NOT NULL,
+  `device` varchar(250) NOT NULL,
+  `time_added` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `games`
+--
+
+INSERT INTO `games` (`id`, `game_title`, `game_desc`, `game_image`, `device`, `time_added`) VALUES
+(34, 'Call of duty', 'Call of duty е мн як FPS', 'df030cf35cff4d9364a94bf3eba2a945.jpg', 'all', '0000-00-00 00:00:00'),
+(33, 'League of legends', 'Не ми се говори', '6d89c7e2d2f242ea81cec2c94f7c6bee.jpeg', 'pc', '0000-00-00 00:00:00'),
+(31, 'Diablo 3', 'Diablo 3 е RPG игра в която си развиваш героя и убиваш чудовища', '26ec11b34dc2045668a5ef70ac7a14b3.png', 'all', '0000-00-00 00:00:00'),
+(32, 'Battlefield 1', 'Battlefield 1 е много як FPS', 'cca0a05b8bf01a50d0f9dd99a152e91d.png', 'all', '0000-00-00 00:00:00'),
+(35, 'Overwatch', 'Много яка', '9abac9265d3786b690d95f7dd89f05dc.jpg', 'all', '2016-08-30 19:36:45');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `msg` text NOT NULL,
+  `color` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `logs`
+--
+
+INSERT INTO `logs` (`id`, `username`, `msg`, `color`) VALUES
+(1, 'gamebg', 'zdr', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `content` text,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `tag` varchar(50) NOT NULL,
+  `author_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `date`, `tag`, `author_id`) VALUES
+(13, 'hfhff', 'gfghfhgk', '2016-08-30 19:04:09', 'PC GAMING', 1),
+(14, 'aaaaa', 'hghjfg', '2016-08-30 19:04:26', 'PC GAMING', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `date_registered` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `username`, `password`, `date_registered`) VALUES
+(1, 'miladinov_1997@abv.bg', 'TheAngelM', 'bea9ddc1815292ef7cc38780324ad97a', '2016-08-02'),
+(5, 'grga@abv.bg', 'tagtag', '78e55a9524a191f7628f82a20bcaa167', '2016-08-07'),
+(4, 'alabala@abv.bg', 'ani', 'djimi', '2016-08-07'),
+(6, 'neshtosi@abv.bg', 'Acho', 'bea9ddc1815292ef7cc38780324ad97a', '2016-08-29');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author_users_id_idx` (`author_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
