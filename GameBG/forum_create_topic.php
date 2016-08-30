0f16403f55da867de5f959d2c0263ff5af8a93c0
Petar Staynov <petar.staynov@gmail.com>
2016-08-30 15:00:46 +0300

    Forum Update

--- GameBG/forum_create_topic.php
+++ GameBG/forum_create_topic.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: PetarStaynov
- * Date: 26.8.2016 г.
- * Time: 19:12
- */
+$title = "Create Topic";
+include 'forum-header.php';
+?>
+
+<?php
+require 'forum_connect.php';
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+{
+//    If user is logged in and is an admin or moderator, he gets a category add field
+    echo '
+        <form method="post" action=""><br>
+        Category name: <input type="text" name="cat_name"/><br>
+        Category description: <textarea name="cat_description"/></textarea><br>
+        <input type="submit" value="Add category" />
+        </form>';
+
+//    Sends category forms text to DB
+    if(isset($_POST["cat_name"]) && isset($_POST["cat_description"])) {
+        $cat_name = $_POST['cat_name'];
+        $cat_description = $_POST['cat_description'];
+
+        $sql = 'INSERT INTO forum_categories (cat_name, cat_description) VALUES ('.$cat_name.', '.$cat_description.')';
+
+    }
+
+    $result = mysqli_query($con, $sql);
+    if (!$result)
+    {
+        echo 'Error' . mysqli_error($con);
+    }
+    else
+    {
+        echo 'New category successfully added.';
+    }
+}
+else
+{
+    echo 'YOU ARE NOT AN ADMIN, GET OUT';
+}
